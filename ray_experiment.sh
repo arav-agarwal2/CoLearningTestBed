99564ea60b09f26dc5de4f507e45b6b907a3845d
Yun Cheng <yuncheng@mars.multicomp.cs.cmu.edu>
2022-06-23 11:49:04 -0400

    partial mosei results

--- ray_experiment.sh
+++ ray_experiment.sh
@@ -7,12 +7,12 @@ HUMOR=/usr0/home/yuncheng/MultiBench/data/humor.pkl
 AVMNIST=/usr0/home/yuncheng/MultiBench/data/avmnist
 
 # Affect experiments
-DATASET=mosi
+DATASET=mosei
 
-for METHOD in mfm
+for METHOD in late_fusion
 do
-    for MODALITIES in 0 1 2 0,1 0,2 1,2 0,1,2
+    for MODALITIES in 0,1,2
     do
-        python3 ray/affect_${METHOD}.py  --modalities [${MODALITIES}] --dataset ${DATASET} --dataset-path ${MOSI} > ray/${DATASET}_logs/${DATASET}_${METHOD}_${MODALITIES}.txt
+        python3 ray/affect_${METHOD}.py  --modalities [${MODALITIES}] --dataset ${DATASET} --dataset-path ${MOSEI} > ray/${DATASET}_logs/${DATASET}_${METHOD}_${MODALITIES}.txt
     done
 done 
