63caa269cdb41a939bb1058c243c96ebf5a45d69
arav-agarwal2 <43820556+arav-agarwal2@users.noreply.github.com>
2022-10-03 16:21:59 -0400

    Update DCCA.py

--- synthetic/DCCA.py
+++ synthetic/DCCA.py
@@ -26,7 +26,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data-path", default="/home/yuncheng/MultiBench/synthetic/SIMPLE_DATA_DIM=3_STD=0.5.pickle", type=str, help="input path of synthetic dataset")
-parser.add_argument("--keys", nargs='+', default=['a','b','c','d','e','label'], type=list, help="keys to access data of each modality and label, assuming dataset is structured as a dict")
+parser.add_argument("--keys", nargs='+', default=['a','b','label'], type=list, help="keys to access data of each modality and label, assuming dataset is structured as a dict")
 parser.add_argument("--modalities", nargs='+', default=[0,1], type=int, help="specify the index of modalities in keys")
 parser.add_argument("--bs", default=32, type=int)
 parser.add_argument("--num-workers", default=4, type=int)
@@ -35,8 +35,10 @@ parser.add_argument("--output-dim", default=128, type=int)
 parser.add_argument("--hidden-dim", default=512, type=int)
 parser.add_argument("--rank", default=32, type=int)
 parser.add_argument("--num-classes", default=2, type=int)
-parser.add_argument("--epochs", default=100, type=int)
+parser.add_argument("--epochs", default=49, type=int)
 parser.add_argument("--weight-decay", default=0.01, type=float)
+parser.add_argument("--lr", default=1e-3, type=float)
+
 parser.add_argument("--saved-model", default='/home/yuncheng/lrf_best.pt', type=str)
 args = parser.parse_args()
 
@@ -51,39 +53,35 @@ train_dataset = torch.utils.data.TensorDataset(*train_tensor_list)
 val_dataset = torch.utils.data.TensorDataset(*test_tensor_list)
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.bs, collate_fn= lambda x: {"views": default_collate(x)})
 val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=args.bs, collate_fn= lambda x: {"views": default_collate(x)})
+trainer = pl.Trainer(
+    max_epochs=args.epochs,
+    enable_checkpointing=False,
+    log_every_n_steps=1,
+    accelerator='cpu'
+)
 
-from info_nce import InfoNCE
+encoders = [MLP(args.input_dim, args.hidden_dim, args.hidden_dim).to(device)
+]*len(args.modalities)
+dcca = DCCA(latent_dims=args.hidden_dim, encoders=encoders)
 
-assert args.modalities == 2
 
-encoder = Linear(args.input_dim, args.output_dim).to(device)
-head = MLP(args.out_dim, args.hidden_dim, args.num_classes).to(device)
-fusion = Concat().cuda()
-criterion = InfoNCE()
-optimizer = torch.optim.Adam(encoder.parameters())
+trainer.fit(dcca, train_dataloader, None)
 
-for idx in range(args.epochs):
-  losses = 0
-  for a,b in train_dataloader:
-    a_enc, b_enc = encoder(a), (b)
-    loss = criterion(a_enc,b_enc)   
-    optimizer.zero_grad()
-    loss.backward()
-    losses += loss.item()
-    optimizer.step()
-  print(losses/len(train_dataloader))
+# freeze parameters
 
-for param in encoder.parameters():
+for encoder in encoders:
+  for param in encoder.parameters():
     param.requires_grad = False
 
-encoders = [encoder for _ in range(2)]
-# Load data
+head = MLP((len(args.keys)-2)*args.hidden_dim, args.hidden_dim, args.num_classes).to(device)
+
+fusion = Concat()
+
+
 traindata, validdata, testdata = get_dataloader(path=args.data_path, keys=args.keys, modalities=args.modalities, batch_size=args.bs, num_workers=args.num_workers)
 
 
 train(encoders, fusion, head, traindata, testdata, args.epochs, optimtype=torch.optim.AdamW, early_stop=False, is_packed=False, lr=args.lr, save=args.saved_model, weight_decay=args.weight_decay, objective=torch.nn.CrossEntropyLoss())
 
-# Testing
-print("Testing:")
 model = torch.load(args.saved_model).to(device)
 test(model, testdata, is_packed=False, no_robust=True, criterion=torch.nn.CrossEntropyLoss())
